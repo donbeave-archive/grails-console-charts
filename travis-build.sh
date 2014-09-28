43ee35991f59ae09b72f1f22aceb378673cb676e
Alexey <donbeave@gmail.com>
2014-09-28 23:11:47 +0800

    Compile GWT modules in Travis CI.

--- travis-build.sh
+++ travis-build.sh
@@ -2,6 +2,7 @@
 set -e
 rm -rf *.zip
 ./grailsw refresh-dependencies --non-interactive
+./grailsw prod compile-gwt-modules
 ./grailsw test-app --non-interactive
 ./grailsw package-plugin --non-interactive
 ./grailsw doc --pdf --non-interactive
