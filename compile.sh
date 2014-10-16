ea167ddfe9d4e1cb3adb9d35fd7729510b588b58
Alexey <donbeave@gmail.com>
2014-10-16 12:11:56 +0800

    Improved compile.sh script.

--- compile.sh
+++ compile.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-rm -rf target/
-grails clean-all
-grails refresh-dependencies
-grails prod compile-gwt-modules
+rm -rf target/ && rm -rf web-app/gwt && grails clean-all && grails refresh-dependencies && grails prod compile-gwt-modules
